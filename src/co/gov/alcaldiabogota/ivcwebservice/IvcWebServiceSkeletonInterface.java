a548cfaed21c25cde59d18b5e736b0c50dc8022c
Jesús Rodríguez Miranda <jesrodmiranda@gmail.com>
2017-01-25 15:17:12 -0500

    Inicio del repositorio para servicios web, versión con campos anteriores IVC

---
+++ src/co/gov/alcaldiabogota/ivcwebservice/IvcWebServiceSkeletonInterface.java
@@ -0,0 +1,22 @@
+/**
+ * IvcWebServiceSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.4  Built on : Oct 21, 2016 (10:47:34 BST)
+ */
+package co.gov.alcaldiabogota.ivcwebservice;
+
+/**
+ *  IvcWebServiceSkeletonInterface java skeleton interface for the axisService
+ */
+public interface IvcWebServiceSkeletonInterface {
+    /**
+     * Auto generated method signature
+     *
+     * @param inIvcWebService
+     * @throws IvcWebServiceExceptionException :
+     */
+    public co.gov.alcaldiabogota.ivcwebservice.InIvcWebServiceResponse inIvcWebService(
+        co.gov.alcaldiabogota.ivcwebservice.InIvcWebService inIvcWebService)
+        throws IvcWebServiceExceptionException;
+}
