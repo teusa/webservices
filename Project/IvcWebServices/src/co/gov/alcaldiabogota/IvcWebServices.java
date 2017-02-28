a947bb766ab7a497785270163bf45e627d817014
Jesús Rodríguez Miranda <jesrodmiranda@gmail.com>
2017-02-28 18:08:09 -0500

    Ajustes para conectarse al servicio web de IVC Confecamaras

--- Project/IvcWebServices/src/co/gov/alcaldiabogota/IvcWebServices.java
+++ Project/IvcWebServices/src/co/gov/alcaldiabogota/IvcWebServices.java
@@ -3,17 +3,39 @@ package co.gov.alcaldiabogota;
 import co.gov.alcaldiabogota.connections.RestClient;
 import co.gov.alcaldiabogota.parameters.PropertiesFile;
 import co.gov.alcaldiabogota.parameters.Stablishment;
+import co.gov.alcaldiabogota.parameters.StablishmentCCB;
 import co.gov.alcaldiabogota.responses.ResponsesIVC;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.apache.axiom.om.*;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.apache.axiom.om.impl.dom.DocumentImpl;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -26,22 +48,22 @@ public class IvcWebServices {
     Properties properties = new Properties();
     PropertiesFile propertiesFile = new PropertiesFile();
     ResponsesIVC ivc = new ResponsesIVC();
-    Stablishment parametersStablishment = new Stablishment();        
+    Stablishment parametersStablishment = new Stablishment();
     RestClient request = new RestClient();
     InputStream input = null;
 
-    public IvcWebServices() throws FileNotFoundException, IOException, Exception {        
+    public IvcWebServices() throws FileNotFoundException, IOException, Exception {
         input = new FileInputStream(propertiesFile.propertiesFile);
         properties.load(input);
     }
-    
+
     /**
      * This method to synchronize and connect soap client from the entities
-     * 
+     *
      * @param requestElement
-     * @return 
+     * @return
      * @throws XMLStreamException
-     * @throws Exception 
+     * @throws Exception
      */
     public OMElement synchronize(OMElement requestElement) throws XMLStreamException, Exception {
         String entity_id = ivc.getRequestParam(requestElement, "entity_id", properties);
@@ -50,28 +72,111 @@ public class IvcWebServices {
         LOGGER.log(Level.INFO, "Entity: {0}, Table: {1}, Date: {2}, Rest: {3}", new Object[]{entity_id, table_name, consult_date, properties.getProperty("RestApiFront")});
         return ivc.responseSoapEntities(entity_id, table_name, consult_date, properties);
     }
-    
+
     /**
-     * This method to build establishment object and connect to REST Server in Yii
-     * 
+     * This method to build establishment object and connect to REST Server in
+     * Yii
+     *
      * @param requestElement
      * @return
-     * @throws XMLStreamException 
+     * @throws XMLStreamException
      */
-    public OMElement establishment(OMElement requestElement) throws XMLStreamException, IOException {        
-        String responseRequest = request.requestRestServer(properties.getProperty("RestApiFront"), parametersStablishment.setStablishmentParameters(requestElement, ivc, properties));        
-        OMNode response = omFactory.createOMText(responseRequest);  
+    public OMElement establishment(OMElement requestElement) throws XMLStreamException, IOException {
+        String responseRequest = request.requestRestServer(properties.getProperty("RestApiFront"), parametersStablishment.setStablishmentParameters(requestElement, ivc, properties));
+        OMNode response = omFactory.createOMText(responseRequest);
         return ivc.createResponse("establishmentResponse", "return", response, properties);
     }
-    
-    
-    public OMElement update( OMElement requestElement) throws IOException {        
+
+    public OMElement update(OMElement requestElement) throws IOException, ParserConfigurationException, SAXException, TransformerException {
         String batch = ivc.getRequestParam(requestElement, "batch", properties);
-        Map<String, String> parameters= null;
-        String restURL = properties.getProperty("RestApiFront") + "?batch="+batch;        
+        Map<String, String> parameters = null;
+        String restURL = properties.getProperty("RestApiFront") + "?batch=" + batch;
         String responseRequest = request.requestRestServer(restURL, parameters);
-        LOGGER.log(Level.INFO, "URL: {0}, Response: {1}", new Object[]{restURL, responseRequest});
-        OMNode response = omFactory.createOMText(responseRequest);  
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
+        Gson gson = gsonBuilder.create();
+
+        StablishmentCCB[] stablishmentCCB = gson.fromJson(responseRequest, StablishmentCCB[].class);
+        List<StablishmentCCB> listStablishment = Arrays.asList(stablishmentCCB);
+        
+        Document xmlDoc = new DocumentImpl();                
+        
+        Element sincronizacionIVC = xmlDoc.createElement("sincronizacionIVC");
+        
+        /**
+         * fechaSincronizacion
+         */
+        Node fechaSincronizacion = xmlDoc.createElement("fechaSincronizacion");
+        fechaSincronizacion.appendChild(xmlDoc.createTextNode("AAAAMMDD"));
+        sincronizacionIVC.appendChild(fechaSincronizacion);
+        
+        /**
+         * tipoSincronizacion
+         */
+        Node tipoSincronizacion = xmlDoc.createElement("tipoSincronizacion");
+        tipoSincronizacion.appendChild(xmlDoc.createTextNode("Masiva"));
+        sincronizacionIVC.appendChild(tipoSincronizacion);
+        
+        /**
+         * numeroCamaraComercio
+         */
+        Node numeroCamaraComercio = xmlDoc.createElement("numeroCamaraComercio");
+        numeroCamaraComercio.appendChild(xmlDoc.createTextNode("04"));
+        sincronizacionIVC.appendChild(numeroCamaraComercio);
+        
+        /**
+         * usuarioAutorizado
+         */
+        Node usuarioAutorizado = xmlDoc.createElement("usuarioAutorizado");
+        usuarioAutorizado.appendChild(xmlDoc.createTextNode("wsbta2017"));
+        sincronizacionIVC.appendChild(usuarioAutorizado);
+        
+        /**
+         * claveUsuario
+         */
+        Node claveUsuario = xmlDoc.createElement("claveUsuario");
+        claveUsuario.appendChild(xmlDoc.createTextNode("bt@*2017"));
+        sincronizacionIVC.appendChild(claveUsuario);
+        
+        /**
+         * numeroRegistros
+         */
+        Node numeroRegistros = xmlDoc.createElement("numeroRegistros");
+        claveUsuario.appendChild(xmlDoc.createTextNode(""));
+        sincronizacionIVC.appendChild(claveUsuario);
+        
+        /**
+         * Multiple node:
+         * datosIdentificacion
+         */                
+        Node datosIdentificacion = xmlDoc.createElement("datosIdentificacion");
+        /*
+        Nombre
+        */
+        Node nombre = xmlDoc.createElement("nombre");
+        nombre.appendChild(xmlDoc.createTextNode(listStablishment.get(0).getName_commercial()));     
+        datosIdentificacion.appendChild(nombre);
+        /*
+        
+        */
+        
+        sincronizacionIVC.appendChild(datosIdentificacion);
+        
+        /**
+         * Save sincronizacionIVC
+         */
+        xmlDoc.appendChild(sincronizacionIVC);
+        
+        StringWriter stringWriter = new StringWriter();
+        Source source = new DOMSource(xmlDoc); 
+         
+        Transformer xformer = TransformerFactory.newInstance().newTransformer();                        
+        xformer.transform(source, new StreamResult(stringWriter));
+
+        LOGGER.log(Level.INFO, "URL: {0}, Response: {1}", new Object[]{restURL, stringWriter.toString()});
+
+        OMNode response = omFactory.createOMText(listStablishment.get(0).getName_commercial());
         return ivc.createResponse("updateResponse", "return", response, properties);
     }
 
---
+++ Project/IvcWebServices/src/co/gov/alcaldiabogota/parameters/StablishmentCCB.java
@@ -0,0 +1,481 @@
+package co.gov.alcaldiabogota.parameters;
+
+/**
+ *
+ * @author jesusrodriguezmiranda
+ */
+public class StablishmentCCB {
+
+    public StablishmentCCB() {
+    }
+
+    private Integer id;
+    private String upz;
+    private String lastname_owner;
+    private String lastname_legal_representative;
+    private String ciiu;
+    private String ciiu2;
+    private String ciiu3;
+    private String address_commercial;
+    private String address_standard;
+    private String address_notification;
+    private String date_end_commercial_registration;
+    private String date_commercial_registration;
+    private String formal;
+    private String digit_verification_establishment;
+    private String digit_verification_owner;
+    private String digit_verification_legal_representative;
+    private String locality;
+    private String email;
+    private String commercial_registration;
+    private String commercial_registration_owner;
+    private String number_identification_establishment;
+    private String number_identification_owner;
+    private String number_identificacion_legal_representative;
+    private String name_commercial;
+    private String name_owner;
+    private String observation;
+    private String observation_history;
+    private String page_web;
+    private String business_name;
+    private String phone;
+    private String type_history;
+    private String type_identification_establishment;
+    private String type_identification_owner;
+    private String type_identification_legal_representative;
+    private String commune;
+    private String name_legal_representative;
+    private String date_last_renewal;
+    private String neighborhood;
+    private String localization_status;
+    private String X_final;
+    private String Y_final;
+    private String category_code;
+    private String subsidiary;
+    private String locality_code;
+    private String locality_name;
+    private String upz_code;
+    private String neighborhood_code;
+    private String phone_two;
+    private String type_establishment_code;
+    private String name_type_establishment;
+    private String name_economic_activity;
+    private String batch;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUpz() {
+        return upz;
+    }
+
+    public void setUpz(String upz) {
+        this.upz = upz;
+    }
+
+    public String getLastname_owner() {
+        return lastname_owner;
+    }
+
+    public void setLastname_owner(String lastname_owner) {
+        this.lastname_owner = lastname_owner;
+    }
+
+    public String getLastname_legal_representative() {
+        return lastname_legal_representative;
+    }
+
+    public void setLastname_legal_representative(String lastname_legal_representative) {
+        this.lastname_legal_representative = lastname_legal_representative;
+    }
+
+    public String getCiiu() {
+        return ciiu;
+    }
+
+    public void setCiiu(String ciiu) {
+        this.ciiu = ciiu;
+    }
+
+    public String getCiiu2() {
+        return ciiu2;
+    }
+
+    public void setCiiu2(String ciiu2) {
+        this.ciiu2 = ciiu2;
+    }
+
+    public String getCiiu3() {
+        return ciiu3;
+    }
+
+    public void setCiiu3(String ciiu3) {
+        this.ciiu3 = ciiu3;
+    }
+
+    public String getAddress_commercial() {
+        return address_commercial;
+    }
+
+    public void setAddress_commercial(String address_commercial) {
+        this.address_commercial = address_commercial;
+    }
+
+    public String getAddress_standard() {
+        return address_standard;
+    }
+
+    public void setAddress_standard(String address_standard) {
+        this.address_standard = address_standard;
+    }
+
+    public String getAddress_notification() {
+        return address_notification;
+    }
+
+    public void setAddress_notification(String address_notification) {
+        this.address_notification = address_notification;
+    }
+
+    public String getDate_end_commercial_registration() {
+        return date_end_commercial_registration;
+    }
+
+    public void setDate_end_commercial_registration(String date_end_commercial_registration) {
+        this.date_end_commercial_registration = date_end_commercial_registration;
+    }
+
+    public String getDate_commercial_registration() {
+        return date_commercial_registration;
+    }
+
+    public void setDate_commercial_registration(String date_commercial_registration) {
+        this.date_commercial_registration = date_commercial_registration;
+    }
+
+    public String getFormal() {
+        return formal;
+    }
+
+    public void setFormal(String formal) {
+        this.formal = formal;
+    }
+
+    public String getDigit_verification_establishment() {
+        return digit_verification_establishment;
+    }
+
+    public void setDigit_verification_establishment(String digit_verification_establishment) {
+        this.digit_verification_establishment = digit_verification_establishment;
+    }
+
+    public String getDigit_verification_owner() {
+        return digit_verification_owner;
+    }
+
+    public void setDigit_verification_owner(String digit_verification_owner) {
+        this.digit_verification_owner = digit_verification_owner;
+    }
+
+    public String getDigit_verification_legal_representative() {
+        return digit_verification_legal_representative;
+    }
+
+    public void setDigit_verification_legal_representative(String digit_verification_legal_representative) {
+        this.digit_verification_legal_representative = digit_verification_legal_representative;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCommercial_registration() {
+        return commercial_registration;
+    }
+
+    public void setCommercial_registration(String commercial_registration) {
+        this.commercial_registration = commercial_registration;
+    }
+
+    public String getCommercial_registration_owner() {
+        return commercial_registration_owner;
+    }
+
+    public void setCommercial_registration_owner(String commercial_registration_owner) {
+        this.commercial_registration_owner = commercial_registration_owner;
+    }
+
+    public String getNumber_identification_establishment() {
+        return number_identification_establishment;
+    }
+
+    public void setNumber_identification_establishment(String number_identification_establishment) {
+        this.number_identification_establishment = number_identification_establishment;
+    }
+
+    public String getNumber_identification_owner() {
+        return number_identification_owner;
+    }
+
+    public void setNumber_identification_owner(String number_identification_owner) {
+        this.number_identification_owner = number_identification_owner;
+    }
+
+    public String getNumber_identificacion_legal_representative() {
+        return number_identificacion_legal_representative;
+    }
+
+    public void setNumber_identificacion_legal_representative(String number_identificacion_legal_representative) {
+        this.number_identificacion_legal_representative = number_identificacion_legal_representative;
+    }
+
+    public String getName_commercial() {
+        return name_commercial;
+    }
+
+    public void setName_commercial(String name_commercial) {
+        this.name_commercial = name_commercial;
+    }
+
+    public String getName_owner() {
+        return name_owner;
+    }
+
+    public void setName_owner(String name_owner) {
+        this.name_owner = name_owner;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public String getObservation_history() {
+        return observation_history;
+    }
+
+    public void setObservation_history(String observation_history) {
+        this.observation_history = observation_history;
+    }
+
+    public String getPage_web() {
+        return page_web;
+    }
+
+    public void setPage_web(String page_web) {
+        this.page_web = page_web;
+    }
+
+    public String getBusiness_name() {
+        return business_name;
+    }
+
+    public void setBusiness_name(String business_name) {
+        this.business_name = business_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getType_history() {
+        return type_history;
+    }
+
+    public void setType_history(String type_history) {
+        this.type_history = type_history;
+    }
+
+    public String getType_identification_establishment() {
+        return type_identification_establishment;
+    }
+
+    public void setType_identification_establishment(String type_identification_establishment) {
+        this.type_identification_establishment = type_identification_establishment;
+    }
+
+    public String getType_identification_owner() {
+        return type_identification_owner;
+    }
+
+    public void setType_identification_owner(String type_identification_owner) {
+        this.type_identification_owner = type_identification_owner;
+    }
+
+    public String getType_identification_legal_representative() {
+        return type_identification_legal_representative;
+    }
+
+    public void setType_identification_legal_representative(String type_identification_legal_representative) {
+        this.type_identification_legal_representative = type_identification_legal_representative;
+    }
+
+    public String getCommune() {
+        return commune;
+    }
+
+    public void setCommune(String commune) {
+        this.commune = commune;
+    }
+
+    public String getName_legal_representative() {
+        return name_legal_representative;
+    }
+
+    public void setName_legal_representative(String name_legal_representative) {
+        this.name_legal_representative = name_legal_representative;
+    }
+
+    public String getDate_last_renewal() {
+        return date_last_renewal;
+    }
+
+    public void setDate_last_renewal(String date_last_renewal) {
+        this.date_last_renewal = date_last_renewal;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getLocalization_status() {
+        return localization_status;
+    }
+
+    public void setLocalization_status(String localization_status) {
+        this.localization_status = localization_status;
+    }
+
+    public String getX_final() {
+        return X_final;
+    }
+
+    public void setX_final(String X_final) {
+        this.X_final = X_final;
+    }
+
+    public String getY_final() {
+        return Y_final;
+    }
+
+    public void setY_final(String Y_final) {
+        this.Y_final = Y_final;
+    }
+
+    public String getCategory_code() {
+        return category_code;
+    }
+
+    public void setCategory_code(String category_code) {
+        this.category_code = category_code;
+    }
+
+    public String getSubsidiary() {
+        return subsidiary;
+    }
+
+    public void setSubsidiary(String subsidiary) {
+        this.subsidiary = subsidiary;
+    }
+
+    public String getLocality_code() {
+        return locality_code;
+    }
+
+    public void setLocality_code(String locality_code) {
+        this.locality_code = locality_code;
+    }
+
+    public String getLocality_name() {
+        return locality_name;
+    }
+
+    public void setLocality_name(String locality_name) {
+        this.locality_name = locality_name;
+    }
+
+    public String getUpz_code() {
+        return upz_code;
+    }
+
+    public void setUpz_code(String upz_code) {
+        this.upz_code = upz_code;
+    }
+
+    public String getNeighborhood_code() {
+        return neighborhood_code;
+    }
+
+    public void setNeighborhood_code(String neighborhood_code) {
+        this.neighborhood_code = neighborhood_code;
+    }
+
+    public String getPhone_two() {
+        return phone_two;
+    }
+
+    public void setPhone_two(String phone_two) {
+        this.phone_two = phone_two;
+    }
+
+    public String getType_establishment_code() {
+        return type_establishment_code;
+    }
+
+    public void setType_establishment_code(String type_establishment_code) {
+        this.type_establishment_code = type_establishment_code;
+    }
+
+    public String getName_type_establishment() {
+        return name_type_establishment;
+    }
+
+    public void setName_type_establishment(String name_type_establishment) {
+        this.name_type_establishment = name_type_establishment;
+    }
+
+    public String getName_economic_activity() {
+        return name_economic_activity;
+    }
+
+    public void setName_economic_activity(String name_economic_activity) {
+        this.name_economic_activity = name_economic_activity;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+}
